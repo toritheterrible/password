d6b32436dda4f8331224c5c1d71fd5ad3c105eff
Tori <torimpham@gmail.com>
2019-12-08 13:20:26 -0800

    added boostrap for design and implemented checkboxes

--- script.js
+++ script.js
@@ -1,196 +1,84 @@
-
-
-
-
-var alphaCapChar = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z"
-  ];
-var alphaLowChar = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z"
-];
-var numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = [
-    
-    "@",
-    "%",
-    "+",
-    "\\",
-    "/",
-    "'",
     "!",
+    "@",
     "#",
     "$",
+    "%",
     "^",
-    "?",
-    ":",
-    ",",
-    ")",
+    "&",
+    "*",
     "(",
-    "}",
-    "{",
-    "]",
-    "[",
-    "~",
+    ")",
     "-",
-    "_",
-    "."
-];
+    "+",
+  ];
 
+  var numbersChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "Z"];
 
-
-
-// function to prompt user for password options
-
-function getPasswordOptions() {
-    // length character variable
-    var length = parseInt(prompt("Please choose your Password Length"));
-
-    if (isNaN(length) === true) {
-        alert("Please pick a real number");
-        return;
-    }
-
-    if (length < 8) {
-        alert("Please choose a value of 8 or higher");
-        return;
-    }
-
-    if (length > 128) {
-        alert("Please choose a value lower than 128");
-        return;
-    }
-
-    // special character variable
-    var hasSpecial = confirm("Would you like to add special characters?");
-    // uppercase character variable
-    var hasUpperCase = confirm("Would you like to add uppercase letters");
-    // lowercase character variable
-    var hasLowerCase = confirm("Would you like to add lowercase letters");
-    // lowercase character variable
-    var hasNum = confirm("Would you like to add numbers?");
-
-    // Conditional statement to make sure user picks one type of character. 
-
-    if (
-      
-        hasNum === false &&
-        hasLowerCase === false &&
-        hasSpecial === false &&
-        hasUpperCase === false
-    ) {
-        alert("Pick at least one character type");
-        return;
-    }
+  
+  
+  function getPasswordOptions() {
+    
+   var hasSpecialChar = document.getElementById("hasSpecialChar").checked;
+   var hasUpperCaseChar = document.getElementById("hasUpperCaseChar").checked;
+   var hasLowerCaseChar = document.getElementById("hasLowerCaseChar").checked;
+   var hasNumbersChar = document.getElementById("hasNumbersChar").checked;
+   var length = document.getElementById("length").value;
    
-    // Object to store user input
+   var passwordOptions = {
+    length: length,
+    hasSpecialChar : hasSpecialChar,
+    hasNumbersChar: hasNumbersChar,
+    hasLowerCaseChar: hasLowerCaseChar,
+    hasUpperCaseChar: hasUpperCaseChar
+};
 
-    var passwordOptions = {
-        length: length,
-        hasSpecial : hasSpecial,
-        hasNum : hasNum,
-        hasLowerCase : hasLowerCase,
-        hasUpperCase : hasUpperCase
-    };
-        return passwordOptions;
+return passwordOptions;
+  }
+  
+  function getRandom(randomString) {
+var randIndex = Math.floor(Math.random() * randomString.length);
+var randElement = randomString[randIndex];
+
+return randElement;
 }
-
-
-// Function for getting a random element from an array
-
-function getRandom(randomString) {
-    var randIndex = Math.floor(Math.random() * randomString.length);
-    var randElement = randomString[randIndex];
-
-    return randElement;
-}
-
-// Function to generate password with user input
-
 function generatePassword() {
-    var options = getPasswordOptions();
+var options = getPasswordOptions();
 
-    var result = [];
+var result = [];
 
-    var possibleCharacters = [];
+var possibleCharacters = [];
 
-    var guaranteedCharacters = [];
+var guaranteedCharacters = [];
 
 
-    if (options.hasSpecial) {
-        possibleCharacters = possibleCharacters.concat(specialChar);
-        guaranteedCharacters.push(getRandom(specialChar));
-    }
-    if (options.hasLowerCase) {
-        possibleCharacters = possibleCharacters.concat(alphaLowChar);
-        guaranteedCharacters.push(getRandom(alphaLowChar));
-    }
-    if (options.hasUpperCase) {
-        possibleCharacters = possibleCharacters.concat(alphaCapChar);
-        guaranteedCharacters.push(getRandom(alphaCapChar));
-    }
-    if (options.hasNum) {
-        possibleCharacters = possibleCharacters.concat(numericChar);
-        guaranteedCharacters.push(getRandom(numericChar));
-    }
+if (options.hasSpecialChar) {
+    possibleCharacters = possibleCharacters.concat(specialChar);
+    guaranteedCharacters.push(getRandom(specialChar));
+}
+if (options.hasLowerCaseChar) {
+    possibleCharacters = possibleCharacters.concat(lowerCaseChar);
+    guaranteedCharacters.push(getRandom(lowerCaseChar));
+}
+if (options.hasUpperCaseChar) {
+    possibleCharacters = possibleCharacters.concat(upperCaseChar);
+    guaranteedCharacters.push(getRandom(upperCaseChar));
+}
+if (options.hasNumbersChar) {
+    possibleCharacters = possibleCharacters.concat(numbersChar);
+    guaranteedCharacters.push(getRandom(numbersChar));
+}
 
 for (var i = 0; i <options.length; i++) {
-    var possibleCharacter = getRandom(possibleCharacters)
+var possibleCharacter = getRandom(possibleCharacters)
 
-    result.push(possibleCharacter);
+result.push(possibleCharacter);
 }
 
 for(var i = 0; i < guaranteedCharacters.length; i++) {
-    result[i] = guaranteedCharacters[i];
+result[i] = guaranteedCharacters[i];
 }
 
 return result.join("");
@@ -201,22 +89,24 @@ var generateBtn = document.querySelector("#generate");
 var copyPassword = document.querySelector("#copy");
 
 function writePassword () {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
+passwordText.value = password;
 
-     //
-     //
-    
+ //
+ //
+
 }
 
 function copyToClipboard() {
-    document.execCommand("copy");
+var passwordText = document.querySelector("#password");
+passwordText.select();
+document.execCommand("copy");
 
-    alert(
-        "Your password " + passwordText.value + " was copied to your clipboard."
-    );
+alert(
+    "Your password " + passwordText.value + " was copied to your clipboard."
+);
 }
 
 generateBtn.addEventListener("click", writePassword);
